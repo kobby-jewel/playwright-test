a2778d8341545872bcb91aa5cf8e731f560125a9
kobby-jewel <48373120+kobby-jewel@users.noreply.github.com>
2023-11-08 06:20:36 +0000

    init

---
+++ tests/test.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+
+test("Check all Hubtel for Hospitals links excluding links with / or wrong format", async ({
+  page,
+  browser,
+}) => {
+  // Navigate to Hubtel for hospitals page
+  await page.goto("https://explore.hubtel.com/request-money-from-anyone/");
+
+  // Find and collect all anchor elements on the page
+  const links = await page.$$eval("a", (elements) =>
+    elements.map((element) => {
+      const href = element.getAttribute("href");
+      return href;
+    })
+  );
+
+  // Loop through each link and check if it's working
+  for (const link of links) {
+    if (link!.startsWith("http://") || link!.startsWith("https://")) {
+      var context1 = await browser.newContext();
+      var page1 = await context1.newPage();
+      var response = await page1.goto(link!, { timeout: 3000 * 10 });
+      await context1.close();
+      var statusCode = await response!.status();
+
+      expect(statusCode).toBe(200);
+    }
+  }
+});
